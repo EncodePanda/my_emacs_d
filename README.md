c2723adeccf6fad31bc6d86c6f3d9b5912d06d71
Paweł Szulc <paul.szulc@gmail.com>
2015-11-22 18:54:10 +0100

    Initialize init.el with repositories, autoupdate & key-chord

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my_emacs_d
 My Emacs configuration, the way I like it
+
+
+Configuration compatible with Emacs version >= 24.4
---
+++ init.el
@@ -0,0 +1,33 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defun package-conditional-install (package-name)
+  "Install a package if it is not present"
+  (unless (package-installed-p package-name)
+  (package-refresh-contents) (package-install package-name)))
+
+(defun packages-conditional-install (packages)
+  "Install list of packages if not present"
+  (when packages
+    (package-conditional-install (car packages))
+    (packages-conditional-install (cdr packages))))
+
+(packages-conditional-install '(auto-package-update key-chord))
+
+(auto-package-update-maybe)
+(key-chord-mode 1)
+
+;;(load "~/.emacs.d/configs/ui")
+;;(load "~/.emacs.d/configs/editing")
+;;(load "~/.emacs.d/configs/windows")
+;;(load "~/.emacs.d/configs/helm")
+;;(load "~/.emacs.d/configs/programmin1g")
+;;(load "~/.emacs.d/configs/scala")
+;;(load "~/.emacs.d/configs/haskell")
+;;(load "~/.emacs.d/configs/other")
