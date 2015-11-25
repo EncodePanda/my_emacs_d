25a48faf7890a63c1ba4958aba20afa3526f4760
Paweł Szulc <paul.szulc@gmail.com>
2015-11-25 21:04:53 +0100

    Add one more shortcut for change window

--- configs/windows.el
+++ configs/windows.el
@@ -1,6 +1,7 @@
 (packages-conditional-install '(ace-window zoom-window))
 
 (global-set-key (kbd "C-c \\") 'ace-window)
+(key-chord-define-global "cw" 'ace-window)
 (key-chord-define-global "sw" 'ace-swap-window)
 (key-chord-define-global "mw" 'ace-maximize-window)
 (key-chord-define-global "zw" 'zoom-window-zoom)
