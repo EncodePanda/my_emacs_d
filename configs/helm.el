510895a6d841375731415f653bb94f7088eda36d
Paweł Szulc <paul.szulc@gmail.com>
2015-11-25 21:04:33 +0100

    Add helm-projectile for files finding

--- configs/helm.el
+++ configs/helm.el
@@ -1,13 +1,16 @@
-(packages-conditional-install '(recentf helm))
+(packages-conditional-install '(recentf helm helm-projectile))
 
 (require 'recentf)
 (setq recentf-max-saved-items 200
       recentf-max-menu-items 15)
 (recentf-mode +1)
 
+(global-set-key (kbd "C-c F") 'helm-projectile-grep)
+(key-chord-define-global "gm" 'helm-projectile-grep)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x b") 'helm-mini)
+(key-chord-define-global "fm" 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (add-hook 'eshell-mode-hook
