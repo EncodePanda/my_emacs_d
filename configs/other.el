52acf0f1ceb8cbcb8df5e341d5ff88acc88a7a3e
Paweł Szulc <paul.szulc@gmail.com>
2015-12-11 23:26:40 +0100

    Add which key mode

--- configs/other.el
+++ configs/other.el
@@ -1,5 +1,6 @@
-(packages-conditional-install '(annoying-arrows-mode csv-mode markdown-mode encourage-mode))
+(packages-conditional-install '(annoying-arrows-mode csv-mode markdown-mode encourage-mode which-key))
 
 (annoying-arrows-mode 1)
 (encourage-mode 1)
+(which-key-mode)
 
