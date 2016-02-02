7011c84daa33b276f6ad3bd347c72ff57a7b4e10
Paweł Szulc <paul.szulc@gmail.com>
2016-02-02 23:03:07 +0100

    Add ag, helm-ag and bind helm-do-ag-project-root

--- configs/helm.el
+++ configs/helm.el
@@ -1,12 +1,12 @@
-(packages-conditional-install '(recentf helm helm-projectile))
+(packages-conditional-install '(recentf ag helm-ag helm helm-projectile))
 
 (require 'recentf)
 (setq recentf-max-saved-items 200
       recentf-max-menu-items 15)
 (recentf-mode +1)
 
-(global-set-key (kbd "C-c F") 'helm-projectile-grep)
-(key-chord-define-global "gm" 'helm-projectile-grep)
+(global-set-key (kbd "C-c F") 'helm-do-ag-project-root)
+(key-chord-define-global "gm" 'helm-do-ag-project-root)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
 (global-set-key (kbd "C-c C-m") 'helm-M-x)
