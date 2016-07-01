c3ba25c09e7117e0469b4ca016402ec92a62fa25
Pawel Szulc <paul.szulc@gmail.com>
2016-07-01 14:20:14 +0200

    Update with awesome helm-projectile integtation

--- configs/helm.el
+++ configs/helm.el
@@ -1,18 +1,25 @@
 (packages-conditional-install '(recentf ag helm-ag helm helm-projectile))
 
 (require 'recentf)
+(require 'projectile)
+(require 'helm-projectile)
 (setq recentf-max-saved-items 200
       recentf-max-menu-items 15)
 (recentf-mode +1)
 
 (global-set-key (kbd "C-c F") 'helm-do-ag-project-root)
-(key-chord-define-global "gm" 'helm-do-ag-project-root)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
 (global-set-key (kbd "C-c C-m") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (key-chord-define-global "fm" 'helm-mini)
+(key-chord-define-global "lm" 'helm-do-ag-this-file)
+(key-chord-define-global "cm" 'helm-projectile-switch-project)
+(key-chord-define-global "pf" 'helm-projectile-find-file)
+(key-chord-define-global "gm" 'helm-do-ag-project-root)
+
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (add-hook 'eshell-mode-hook
