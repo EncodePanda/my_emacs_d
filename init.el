30007d1a5024bd6845dfe7d256cad99ad4258250
Pawel Szulc <rabbit@Pawels-MacBook-Pro.local>
2016-07-18 11:42:19 +0200

    Add put-file-name-on-clipboard

--- init.el
+++ init.el
@@ -52,3 +52,15 @@
 (load "~/.emacs.d/configs/erlang")
 (load "~/.emacs.d/configs/other")
 (load "~/.emacs.d/configs/org")
+
+(defun my-put-file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
