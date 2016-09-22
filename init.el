7d79ed256f578d9221e1c5416f64c39c4de7e009
Pawel Szulc <paul.szulc@gmail.com>
2016-09-22 13:16:35 +0200

    Batch config changes
    Configure: greek letters, yasnippet, backup.
    Add elmacro
    Fix gitter messing with lin numbers - required pushing 'programming.el'
    up in in init.el config

--- init.el
+++ init.el
@@ -18,13 +18,31 @@
     (package-conditional-install (car packages))
     (packages-conditional-install (cdr packages))))
 
-(packages-conditional-install '(auto-package-update key-chord keyfreq eno company nyan-mode exec-path-from-shell))
+(packages-conditional-install '(auto-package-update key-chord keyfreq eno company nyan-mode exec-path-from-shell elmacro yasnippet))
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
 (auto-package-update-maybe)
 (key-chord-mode 1)
+
+(require 'yasnippet)
+
+
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"
+        "~/.emacs.d/my_snippets"
+	))
+
+(yas-global-mode 1)
+
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
 
 ;; mac specific settings
 (when (eq system-type 'darwin) 
@@ -46,14 +64,16 @@
 
 (load "~/.emacs.d/configs/ui")
 (load "~/.emacs.d/configs/editing")
+(load "~/.emacs.d/configs/programming")
 (load "~/.emacs.d/configs/git")
 (load "~/.emacs.d/configs/windows")
 (load "~/.emacs.d/configs/helm")
-(load "~/.emacs.d/configs/programming")
+
 ;; (load "~/.emacs.d/configs/scala")
 (load "~/.emacs.d/configs/erlang")
 (load "~/.emacs.d/configs/other")
 (load "~/.emacs.d/configs/org")
+(load "~/.emacs.d/configs/greek")
 
 (defun my-put-file-name-on-clipboard ()
   "Put the current file name on the clipboard"
