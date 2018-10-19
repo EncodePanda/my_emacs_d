81e88910a153263845509250a67f6decfc0c3a83
Pawel Szulc <paul.szulc@gmail.com>
2018-10-19 19:17:54 +0200

    Simplify scala.el

--- configs/scala.el
+++ configs/scala.el
@@ -1,66 +1,15 @@
-(packages-conditional-install '(ensime scala-mode yasnippet))
+(packages-conditional-install '(sbt-mode scala-mode))
+;; bug in sbt-mode fixed 
+(setq sbt:prefer-nested-projects t)
 
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(add-hook 'sbt-mode-hook
+          (lambda ()
+            (setq prettify-symbols-alist
+                  `((,(expand-file-name (directory-file-name default-directory)) . ?⌂)
+                    (,(expand-file-name "~") . ?~)))
+            (prettify-symbols-mode t)))
 
-(require 'yasnippet)
-
-
-(setq yas-snippet-dirs
-      '("~/.emacs.d/snippets"
-        "~/.emacs.d/my_snippets"
-	))
-(yas-reload-all)
-(add-hook 'scala-mode-hook #'yas-minor-mode)
-(with-eval-after-load 'company
-  (define-key company-active-map [tab] nil)
-  )
-
-(add-hook 'scala-mode-hook #'linum-mode)
-
-;; ignore first test
-(defun ignore-fst-test ()
-  "Change me!"
-  (interactive)
-  (beginning-of-buffer nil)
-  (isearch-forward nil 1)
-  (isearch-printing-char 116 1)
-  (isearch-printing-char 101 1)
-  (isearch-printing-char 115 1)
-  (isearch-printing-char 116 1)
-  (isearch-printing-char 40 1)
-  (isearch-printing-char 34 1)
-  (isearch-exit)
-  (forward-word 1)
-  (backward-word 1)
-  (backward-word 1)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (insert "ignore"))
-
-(defun unignore-fst-test ()
-  "Change me!"
-  (interactive)
-  (beginning-of-buffer nil)
-  (isearch-forward nil 1)
-  (isearch-printing-char 105 1)
-  (isearch-printing-char 103 1)
-  (isearch-printing-char 110 1)
-  (isearch-printing-char 111 1)
-  (isearch-printing-char 114 1)
-  (isearch-printing-char 101 1)
-  (isearch-printing-char 40 1)
-  (isearch-printing-char 34 1)
-  (isearch-exit)
-  (backward-word 1)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (delete-forward-char 1 nil)
-  (insert "test"))
-
+(require 'sbt-mode)
+(setq compilation-auto-jump-to-first-error t)
+(global-set-key (kbd "C-c s") 'sbt-hydra)
 
