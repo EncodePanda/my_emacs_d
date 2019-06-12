a21f9d0a51089a18c458ec28abe62e8bae3ec0a8
Pawel Szulc <paul.szulc@gmail.com>
2019-06-12 16:09:03 +0200

    Haskell flymake hlint hints enabled

--- configs/haskell.el
+++ configs/haskell.el
@@ -1,2 +1,5 @@
 (packages-conditional-install '(haskell-mode intero hasky-stack))
 (require 'hasky-stack)
+
+;;(require 'flymake-hlint) ;; not needed if installed via package
+;;(add-hook 'haskell-mode-hook 'flymake-hlint-load)
