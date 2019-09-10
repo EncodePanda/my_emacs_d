9a12e961e23a0a8ad1a2ae4853b1dc6dd5f236d3
Pawel Szulc <paul.szulc@gmail.com>
2019-09-10 22:52:09 +0200

    Add haskell stylish on save

--- configs/haskell.el
+++ configs/haskell.el
@@ -1,5 +1,8 @@
 (packages-conditional-install '(haskell-mode intero hasky-stack))
 (require 'hasky-stack)
 
+(custom-set-variables
+ '(haskell-stylish-on-save t))
+
 ;;(require 'flymake-hlint) ;; not needed if installed via package
 ;;(add-hook 'haskell-mode-hook 'flymake-hlint-load)
