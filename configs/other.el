94a770320a0dadda9ffc29513e5e015de8ea3370
Pawel Szulc <paul.szulc@gmail.com>
2019-12-02 00:13:15 +0100

    Configure which-key

--- configs/other.el
+++ configs/other.el
@@ -1,6 +1,4 @@
-(packages-conditional-install '(annoying-arrows-mode csv-mode markdown-mode encourage-mode which-key))
+(packages-conditional-install '(annoying-arrows-mode csv-mode markdown-mode encourage-mode))
 
 (annoying-arrows-mode 1)
 (encourage-mode 1)
-(which-key-mode)
-
