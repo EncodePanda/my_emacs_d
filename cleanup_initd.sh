594fef8ae798136f4c5b8874c29fddbc69b39cee
Pawel Szulc <pawel.szulc@klarna.com>
2020-05-14 09:43:02 +0200

    Add script that cleans up the init.d

---
+++ cleanup_initd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ~/.emacs.d/auto-save-list/
+rm -rf ~/.emacs.d/elpa/
+rm -rf ~/.emacs.d/transient/
+rm -rf ~/.emacs.d/backup/
+rm -rf ~/.emacs.d/snippets/
+rm ~/.emacs.d/projectile-bookmarks.eld
+rm ~/.emacs.d/recentf
+rm ~/.emacs.d/.last-package-update-day
