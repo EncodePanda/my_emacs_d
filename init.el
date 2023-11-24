23bb5a0251d72f76cadaa600b19e5e2b051b8bae
EncodePanda <paul.szulc@gmail.com>
2023-11-24 09:51:47 +0100

    Dired guess target diretory

--- init.el
+++ init.el
@@ -325,6 +325,11 @@
   "pc" 'project-compile                   ;; compile current project
 )
 
+;; Dired tries to guess a default target directory. This means if there is a
+;; Dired buffer displayed in some window, use its current directory, instead of
+;; this Dired buffer's current directory.
+(setq dired-dwim-target t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Navigation
