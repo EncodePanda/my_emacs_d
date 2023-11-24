2f6d5d897d233c45f93cd233cd586bcb73d6df8e
EncodePanda <paul.szulc@gmail.com>
2023-11-24 09:52:11 +0100

    Configure dashboard

--- init.el
+++ init.el
@@ -421,8 +421,12 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package dashboard
-  :config
-  (dashboard-setup-startup-hook))
+  :config (dashboard-setup-startup-hook))
+          (setq dashboard-items '((recents  . 5)
+          		       (bookmarks . 5)
+          		       (projects . 5)
+          		       (agenda . 5)
+          		       (registers . 5)))
 (setq dashboard-banner-logo-title "Welcome back, Encode Panda")
 (setq dashboard-startup-banner "~/.emacs.d/logo.png")
 (push 'dashboard-mode evil-insert-state-modes)
